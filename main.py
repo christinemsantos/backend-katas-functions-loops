5f884e577e96b48f3a4f067c2c512faa75aaed4e
Christine Santos <christinemsantos1@gmail.com>
2020-03-10 11:02:21 -0400

    Completes Katas

--- main.py
+++ main.py
@@ -6,33 +6,46 @@ __author__ = "???"
 
 def add(x, y):
     """Add two integers. Handles negative values."""
-    # your code here
-    return
-
+    sum = x + y
+    return sum
+print add(2,3)
 
 def multiply(x, y):
     """Multiply x with y. Handles negative values of x or y."""
-    # your code here
-    return
-
+    result = 0
+    for a in range(abs(y)):
+        result = add(x,result)
+    if y < 0 :
+        return -result
+    else:
+        return result
+print multiply(6,-8)
 
 def power(x, n):
     """Raise x to power n, where n >= 0"""
-    # your code here
-    return
+    result = 1
+    for b in range(n):
+        result = multiply(x,result)
+    return result
+print power(2,8)
 
 
 def factorial(x):
     """Compute factorial of x, where x > 0"""
-    # your code here
-    return
+    result = 1
+    for c in range(2,add(x,1)):
+        result = multiply(c,result)
+    return result
+print factorial(4)
 
 
 def fibonacci(n):
     """Compute the nth term of fibonacci sequence"""
-    # your code here
-    return
-
+    d, e = 0, 1
+    for _ in range(1,n):
+        d, e = e, add(d,e)
+    return e
+print fibonacci(8)
 
 if __name__ == '__main__':
     # your code to call functions above
